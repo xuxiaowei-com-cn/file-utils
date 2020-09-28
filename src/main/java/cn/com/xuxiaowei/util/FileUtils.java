d490b370476cf0a5ce832b20754b84f5862543a3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-28 18:59:05 +0800

    读取文件

--- src/main/java/cn/com/xuxiaowei/util/FileUtils.java
+++ src/main/java/cn/com/xuxiaowei/util/FileUtils.java
@@ -15,7 +15,9 @@
  */
 package cn.com.xuxiaowei.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.List;
 
 /**
@@ -90,6 +92,33 @@ public class FileUtils {
         }
 
         file(file, fileList, directory);
+    }
+
+    /**
+     * 读取文件
+     *
+     * @param file 文件
+     * @return 返回 文件内容
+     */
+    public static String readerFile(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            // 创建一个新FileReader，给定从中读取文件 。
+            FileReader fileReader = new FileReader(file);
+
+            // 创建一个使用默认大小输入缓冲器的缓冲字符输入流。
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String readLine;
+            // 读取文本行。 的线被认为是由一个进料线中的任何一个被终止（“\n”），回车（“\r”），或回车立即由换行遵循。
+            while ((readLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(System.lineSeparator()).append(readLine);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
     }
 
 }
